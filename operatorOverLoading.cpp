73bfa236c9bf08ac1fadf7bd8d6e0e5676a0665d
devCynosure <sharanprakash.sfc@gmail.com>
2021-12-01 22:30:56 +0530

    Do Operator Overloading

--- operatorOverLoading.cpp
+++ operatorOverLoading.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
 
-class complex{
+class Complex{
     private:
-    int real, img;
-    complex(int a, int b)a()
+    int real, imag;
+    public:
+    Complex(int a=0, int b=0):real(a),imag(b){};
+    Complex operator+(Complex const &obj){
+        Complex res;
+        res.real = real + obj.real;
+        res.imag = imag + obj.imag;
+        return res;
 
-}
+        
+    }
+    void print(){ cout<<real<<"+i"<<imag<<endl;}
+
+};
 
 int main(){
-    complex c1(10,20), c2(20, 40);
+    Complex c1(10,20), c2(20, 40);
+    Complex c3 = c1 + c2;
+    c3.print();
 
 return 0;
 }
